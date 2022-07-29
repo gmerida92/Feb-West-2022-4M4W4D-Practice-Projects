34c702b6db15f4a8bac6df4f58e72481fc1e7779
Daniel Yoo <dacapo0509@gmail.com>
2022-07-28 21:07:56 -0700

    part 1 done

--- practice-for-week-11-sequelize-aggregate-data-short-practice/server/app.js
+++ practice-for-week-11-sequelize-aggregate-data-short-practice/server/app.js
@@ -1,116 +1,110 @@
 // Instantiate Express and the application - DO NOT MODIFY
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Import environment variables in order to connect to database - DO NOT MODIFY
-require('dotenv').config();
-require('express-async-errors');
-
+require("dotenv").config();
+require("express-async-errors");
 
 // Import the models used in these routes - DO NOT MODIFY
-const { Cat, Toy, sequelize } = require('./db/models');
+const { Cat, Toy, sequelize } = require("./db/models");
 const { Op } = require("sequelize");
-
 
 // Express using json - DO NOT MODIFY
 app.use(express.json());
 
-
-
-
 // STEP 1: Load the toys and find the count, min price, max price, and sum
-app.get('/toys', async (req, res, next) => {
+app.get("/toys", async (req, res, next) => {
+	// A. Create an `allToys` variable that returns all toys
+	const allToys = await Toy.findAll();
 
-    // A. Create an `allToys` variable that returns all toys
-    // Your code here
+	// B. Create a `toysCount` variable that returns the total number of toy
+	// records
+	// Your code here
 
-    // B. Create a `toysCount` variable that returns the total number of toy
-    // records
-    // Your code here
-    
-    // C. Create a `toysMinPrice` variable that returns the minimum price of all
-    // the toys
-    // Your code here
-    
-    // D. Create a `toysMaxPrice` variable that returns the maximum price of all
-    // the toys
-    // Your code here
+	// const toysCount = await allToys.count(); ONLY WORKS WITH MODEL METHOD
+	const toysCount = await Toy.count();
+	// C. Create a `toysMinPrice` variable that returns the minimum price of all
+	// the toys
+	// Your code here
 
-    // E. Create a `toysSumPrice` variable that returns the sum of all of
-    // the toy prices.
-    // Your code here
+	const toysMinPrice = await Toy.min("price");
+	// D. Create a `toysMaxPrice` variable that returns the maximum price of all
+	// the toys
+	// Your code here
 
-    res.json({
-        toysCount,
-        toysMinPrice,
-        toysMaxPrice,
-        toysSumPrice,
-        allToys
-    });
+	const toysMaxPrice = await Toy.max("price");
+	// E. Create a `toysSumPrice` variable that returns the sum of all of
+	// the toy prices.
+	// Your code here
+
+	const toysSumPrice = await Toy.sum("price");
+	res.json({
+		toysCount,
+		toysMinPrice,
+		toysMaxPrice,
+		toysSumPrice,
+		allToys,
+	});
 });
 
-
-
-
 // STEP 2a: Find a cat with their associated toys, and aggregate toy data
-app.get('/cats/:id/toys', async (req, res, next) => {
+app.get("/cats/:id/toys", async (req, res, next) => {
+	const catToysAggregateData = await Cat.findByPk(req.params.id, {
+		include: {
+			model: Toy,
+			attributes: [],
+		},
+		attributes: [
+			//When specifying column, make sure to use plural ref TOYS
 
-    const catToysAggregateData = await Cat.findByPk(req.params.id, {
-        include: {
-            model: Toy,
-            attributes: []
-        },
-        attributes: [
-            // Count all of this cat's toys, and display the value with a
-            // key of `toyCount`
-            // Your code here
+			// Count all of this cat's toys, and display the value with a
+			// key of `toyCount`
+			[sequelize.fn("COUNT", sequelize.col("Toys.id")), "toyCount"],
+			// Find the average price of this cat's toys, and display the
+			// value with a key of `averageToyPrice`
+			[sequelize.fn("AVG", sequelize.col("Toys.price")), "averageToyPrice"],
+			// Find the total price of this cat's toys, and display the
+			// value with a key of `totalToyPrice`
+			[sequelize.fn("TOTAL", sequelize.col("Toys.price")), "totalToyPrice"],
+		],
+		raw: true,
+	});
 
-            // Find the average price of this cat's toys, and display the
-            // value with a key of `averageToyPrice`
-            // Your code here
+	const cat = await Cat.findByPk(req.params.id, {
+		include: { model: Toy },
+	});
 
-            // Find the total price of this cat's toys, and display the
-            // value with a key of `totalToyPrice`
-            // Your code here
-        ],
-        raw: true
-    });
+	// STEP 2b: Format the cat object to add the aggregate keys and values to it
 
-    const cat = await Cat.findByPk(req.params.id, {
-        include: { model: Toy }
-    });
-    
+	// Define a new variable, `catData`, and set it equal to the `cat` variable converted to JSON
+	// Your code here
 
-    // STEP 2b: Format the cat object to add the aggregate keys and values to it
+	const catData = cat.toJSON(); // changes object to json object
 
-    // Define a new variable, `catData`, and set it equal to the `cat` variable converted to JSON 
-    // Your code here
+	// Add the `toyCount`, `averageToyPrice`, and `totalToyPrice` keys to the
+	// catData object, with their aggregate values from `catToysAggregateData`
+	// Your code here
+	catData.toyCount = catToysAggregateData.toyCount;
+	catData.averageToyPrice = catToysAggregateData.averageToyPrice;
+	catData.totalToyPrice = catToysAggregateData.totalToyPrice;
 
-    // Add the `toyCount`, `averageToyPrice`, and `totalToyPrice` keys to the
-    // catData object, with their aggregate values from `catToysAggregateData`
-    // Your code here
-
-
-    // After the steps above are complete, refactor the line below to only
-    // display `catData`
-    res.json({ catToysAggregateData, cat });
-})
-
-
+	// After the steps above are complete, refactor the line below to only
+	// display `catData`
+	res.json({ catData });
+});
 
 // BONUS STEP: Create an endpoint for GET /data-summary that includes a summary
 // of all the aggregate data according to spec
 // Your code here
 
-
-
 // Root route - DO NOT MODIFY
-app.get('/', (req, res) => {
-    res.json({
-        message: "API server is running"
-    });
+app.get("/", (req, res) => {
+	res.json({
+		message: "API server is running",
+	});
 });
 
 // Set port and listen for incoming requests - DO NOT MODIFY
-const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+const port = 8000;
+app.listen(port, () => console.log("Server is listening on port", port));
